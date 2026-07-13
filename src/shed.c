04476c547866ae8621edc1b87304d9f89d84996e
Simon Long <simon@raspberrypi.com>
2026-07-13 11:40:27 +0100

    Remove wayfire references

--- src/shed.c
+++ src/shed.c
@@ -48,7 +48,6 @@ extern const char *dgetfixt (const char *domain, const char *msgctxid);
 
 typedef enum {
     WM_OPENBOX,
-    WM_WAYFIRE,
     WM_LABWC } 
 wm_type;
 
@@ -1266,11 +1265,7 @@ void init_plugin (GtkWidget *parent)
     bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
     textdomain (GETTEXT_PACKAGE);
 
-    if (getenv ("WAYLAND_DISPLAY"))
-    {
-        if (getenv ("WAYFIRE_CONFIG_FILE")) wm = WM_WAYFIRE;
-        else wm = WM_LABWC;
-    }
+    if (getenv ("WAYLAND_DISPLAY")) wm = WM_LABWC;
     else wm = WM_OPENBOX;
 
     main_dlg = parent;
